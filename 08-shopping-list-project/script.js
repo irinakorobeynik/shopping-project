a672da69236774e383bd4c94e29b9a6cd1c67e9d
k0robeynik <IrinaKorobeynik@coherentsolutions.com>
2024-12-30 15:12:50 +0300

    Adding init function, adding storing items into local storage

--- 08-shopping-list-project/script.js
+++ 08-shopping-list-project/script.js
@@ -4,6 +4,13 @@ const itemList = document.querySelector('#item-list');
 const clearBtn = document.querySelector('#clear');
 const filter = document.querySelector('#filter');
 
+function displayItems() {
+    const itemsFromStorage = getItemsFromStorage();
+    itemsFromStorage.forEach(item => addItemToDOM(item));
+
+    checkUI();
+}
+
 function createIcon(classes) {
     const i = document.createElement('i');
     i.setAttribute('class', classes);
@@ -19,7 +26,7 @@ function createButton(classes) {
 }
 
 
-function addItem(e) {
+function onAddItemSubmit(e) {
     e.preventDefault();
 
     const newItem = itemInput.value;
@@ -27,13 +34,8 @@ function addItem(e) {
         alert('Please add an Item');
         return;
     }
-    console.log('success');
-
-    const listItem = document.createElement('li');
-    listItem.appendChild(document.createTextNode(newItem));
-    const button = createButton('remove-item btn-link text-red');
-    listItem.appendChild(button);
-    itemList.appendChild(listItem);
+    addItemToDOM(newItem);
+    addItemToStorage(newItem);
     checkUI();
     itemInput.value = '';
     
@@ -66,9 +68,8 @@ function filterItems(e) {
             item.style.display = 'flex';
         } else {
             item.style.display = 'none';
-            checkUI();
-        }
-        
+            
+        }    
     })
 }
 
@@ -83,12 +84,39 @@ const item = itemList.querySelectorAll('li');
     }
 }
 
+function addItemToDOM(item) {
+    const listItem = document.createElement('li');
+    listItem.appendChild(document.createTextNode(item));
+    const button = createButton('remove-item btn-link text-red');
+    listItem.appendChild(button);
+    itemList.appendChild(listItem);
+}
 
+function addItemToStorage(item) {
+    const itemsFromStorage = getItemsFromStorage();
+    itemsFromStorage.push(item);
 
-// Event Listeners
-itemForm.addEventListener('submit', addItem);
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function getItemsFromStorage() {
+        let itemsFromStorage;
+    if (localStorage.getItem('items') === null){
+        itemsFromStorage = [];
+    } else {
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+    }
+    return itemsFromStorage;
+    
+}
+
+function init(params) {
+    itemForm.addEventListener('submit', onAddItemSubmit);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', removeAllItem);
-filter.addEventListener('input', filterItems)
-
+filter.addEventListener('input', filterItems);
+document.addEventListener('DOMContentLoaded', displayItems)
 checkUI();
+}
+
+init();
